901ac484684ac7dc3d545c3a26087ac4886b2a55
Luka Mlinarič <j6.bountyhounter@gmail.com>
2023-12-03 11:59:19 +0100

    Add Cypress and start-server-and-test dependencies

---
+++ cypress/e2e/newNote.cy.ts
@@ -0,0 +1,21 @@
+describe('Creating a new note', () => {
+  it('should create a new note', () => {
+    cy.viewport(1920, 1080);
+    cy.visit('http://localhost:3000');
+    cy.wait(1000);
+    cy.get('button.inline-flex:nth-child(3)').click();
+    cy.wait(3000);
+    cy.get(
+      'div.flex:nth-child(4) > div:nth-child(1) > button:nth-child(2)',
+    ).click();
+    cy.get('input.bg-transparent').clear();
+    cy.get('input.bg-transparent').type('test');
+    cy.get('button.px-2:nth-child(1)').click();
+    cy.wait(2000);
+    cy.get('inactive-text hover:text-white').click();
+    cy.get('button.inline-flex:nth-child(1)').click();
+    cy.get(
+      'div.border:nth-child(2) > div:nth-child(1) > h2:nth-child(1)',
+    ).contains('New Note');
+  });
+});
